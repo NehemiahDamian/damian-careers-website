01e36b27931634389dfcfdd2bf791a851e6359bd
Nehemiah Damian <84781708+NehemiahDamian@users.noreply.github.com>
2024-04-17 17:00:35 +0000

    Added requirements.txt

--- app.py
+++ app.py
@@ -3,39 +3,41 @@ from flask import Flask, render_template, jsonify
 app = Flask(__name__)
 
 JOBS = [
-  {
-    "id":1,
-    "title": "Data Analyst",
-    "location": "Bengaluru, India",
-    "Salary": "Rs. 10,00,000"
-  },
-  {
-    "id":2,
-    "title": "Data Analyst",
-    "location": "Bengaluru, India",
-    "Salary": "Rs. 10,00,000"
-  },
-  {
-    "id":3,
-    "title": "Data Analyst",
-    "location": "Bengaluru, India"
-  },
-  {
-    "id":4,
-    "title": "Data Analyst",
-    "location": "Bengaluru, India",
-    "Salary": "Rs. 10,00,000"
-  },
+    {
+        "id": 1,
+        "title": "Data Analyst",
+        "location": "Bengaluru, India",
+        "Salary": "Rs. 10,00,000"
+    },
+    {
+        "id": 2,
+        "title": "Data Analyst",
+        "location": "Bengaluru, India",
+        "Salary": "Rs. 10,00,000"
+    },
+    {
+        "id": 3,
+        "title": "Data Analyst",
+        "location": "Bengaluru, India"
+    },
+    {
+        "id": 4,
+        "title": "Data Analyst",
+        "location": "Bengaluru, India",
+        "Salary": "Rs. 10,00,000"
+    },
 ]
+
 
 @app.route('/api/jobs')
 def lis_jobs():
   return jsonify(JOBS)
 
+
 @app.route("/")
 def hello_world():
   return render_template("home.html", jobs=JOBS)
 
+
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
-  
